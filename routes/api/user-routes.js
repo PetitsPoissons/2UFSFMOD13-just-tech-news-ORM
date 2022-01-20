ba747312510bbaf2eb811271c76901c59a5fb545
Sandrine Poissonnet <petitspoissons@icloud.com>
2022-01-20 05:58:12 -0800

    login complete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,10 +52,37 @@ router.post('/', (req, res) => {
 		});
 });
 
+// POST /api/users/login
+router.post('/login', (req, res) => {
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	})
+		.then((dbUserData) => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this email' });
+				return;
+			}
+			// verify user
+			const validPassword = dbUserData.checkPassword(req.body.password);
+			if (!validPassword) {
+				res.status(400).json({ message: 'Incorrect password!' });
+				return;
+			}
+			res.json({ user: dbUserData, message: 'You are now logged in!' });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
 	// if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
 	User.update(req.body, {
+		individualHooks: true,
 		where: {
 			id: req.params.id,
 		},
